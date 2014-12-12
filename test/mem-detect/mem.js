db6ac2aebd4222188dbed98b0dc027b6311a5141
Hunter Loftis <hunter@hunterloftis.com>
2014-12-11 18:24:03 -0800

    test and implementation for memory detection

--- test/mem-detect/mem.js
+++ test/mem-detect/mem.js
@@ -1,5 +1,6 @@
 var available = process.env.MEM_AVAILABLE;
 var allowance = process.env.MEM_ALLOWANCE;
 var count = process.env.PROCESS_COUNT;
+var gc = !!global.gc;
 
-console.log([available, allowance, count].join(' '));
+console.log([available, allowance, count, gc].join(' '));
